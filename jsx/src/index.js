5830250839b7eb1e31257d2c8e68ea6bd730332a
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-06 19:01:48 -0400

    master

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,15 +2,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const getButtonText = () => {
+  return "Click on me!";
+};
+
+// Noramlly any component with any content like this one would not be in the index.js file. I would at minimum be in the app.js file component.
 // Create a React Component
 const App = () => {
+  const buttonText = { text: "click me" };
+  const labelText = "Enter Name:";
+  const style = {
+    backgroundColor: "blue",
+    color: "white",
+  };
   return (
     <div>
-      <label className="label" for="name">
-        Enter Name:
+      <label className="label" htmlFor="name">
+        {labelText}
       </label>
       <input id="name" type="text" />
-      <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+      <button style={style}>{buttonText.text}</button>
     </div>
   );
 };
